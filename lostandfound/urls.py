30005a6124ee4bb149aa3f34a4a57761e1c2c8fe
Himaghna <pen314paper@gmail.com>
2026-06-03 23:35:03 -0400

    Redesign emails to match website style, fix password reset HTML rendering

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -18,7 +18,8 @@ urlpatterns = [
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
              template_name='registration/password_reset.html',
-             email_template_name='registration/password_reset_email.html',
+             email_template_name='registration/password_reset_email.txt',
+             html_email_template_name='registration/password_reset_email.html',
              subject_template_name='registration/password_reset_subject.txt',
          ),
          name='password_reset'),
